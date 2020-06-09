e903900feb4bb6816e779c746fb5d4f2ef636863
Vasiliy <kkolyasev1228@gmail.com>
2020-06-09 16:32:02 +0300

    regular

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -22,7 +22,8 @@ class Router {
     public static function matchRoute ($url)
     {
         foreach (self::$routes as $pattern=>$route) {
-            if ($pattern == $url) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                debug($matches);
                 self::$route = $route;
                 return true;
             }
