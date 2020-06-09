3924c2fde6873e8d93cb341086a229e4c9eab970
Vasiliy <kkolyasev1228@gmail.com>
2020-06-09 17:34:15 +0300

    not controller

--- public/index.php
+++ public/index.php
@@ -22,6 +22,9 @@ spl_autoload_register (function ($class)
 }
 );
 
+Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'Posts']);
+
+//defaults routs
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
