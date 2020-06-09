c6341f62e7759cd4dbef1b40aba569584df77577
Vasiliy <kkolyasev1228@gmail.com>
2020-06-09 17:16:38 +0300

    case

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
+require '../app/controllers/Main.php';
+require '../app/controllers/Posts.php';
+require '../app/controllers/PostsNew.php';
 
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
