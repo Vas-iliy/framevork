9180484dbd0a54b84d07b4359221bb639ec6789d
Vasiliy <kkolyasev1228@gmail.com>
2020-06-11 22:58:08 +0300

    get params in URL

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -75,6 +75,7 @@ class Router {
      */
     public static function dispatch ($url)
     {
+        $url = self::removeQueryString($url);
         if (self::matchRoute($url)) {
             $controller = 'app\controllers\\' . self::$route['controller'];
             if (class_exists($controller)) {
@@ -102,6 +103,18 @@ class Router {
     protected static function lowerCamelCase ($name)
     {
         return lcfirst(self::upperCamelCase($name));
+    }
+    
+    protected static function removeQueryString ($url)
+    {
+        if ($url) {
+            $params = explode('&', $url, 2);
+            if (false === strpos($params[0], '=')) {
+                return rtrim($params[0], '/');
+            } else {
+                return '';
+            }
+        }
     }
 
 }
