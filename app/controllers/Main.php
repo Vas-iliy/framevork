84eb69c3ede919e21ab26fe02fafb63a021fb8f2
Vasiliy <kkolyasev1228@gmail.com>
2020-06-13 00:22:50 +0300

    new views and layouts in main

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -4,7 +4,11 @@ namespace app\controllers;
 
 class Main extends App
 {
+    public $layout = 'main';
+
     public function indexAction ()
     {
+//        $this->layout = 'main';
+//        $this->view = 'test';
     }
 }
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -13,6 +13,9 @@
 <body>
     <h1>Hello, world!</h1>
 
-    <script src="/bootstrap/js/bootstrap.js"></script>
+    <?=$content?>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="/public/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
